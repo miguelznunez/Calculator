3613fc8a997b1299f57534f309ffe0ae3d87b505
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-29 21:41:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const userInput =  document.getElementById("userInput");
-var expression = '';
+const userInput =  document.querySelector("#user_input");
+var expression = "";
 
-function press(num){
+press = (num) => {
   expression += num;
   userInput.value = expression;
 }
 
-function equal(){
+equal = () => {
   userInput.value = eval(expression);
-  expression = '';
+  expression = "";
 }
 
-function erase(){
-  expression = '';
+erase = () => {
+  expression = "";
   userInput.value = expression;
 }
